5e8bef934d7b2be330b18649371a819b92265fa4
technolog <technologies.bro@gmail.com>
2020-02-27 20:49:50 +0300

    main

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import RouterPrefetch from 'vue-router-prefetch'
+import DashboardPlugin from './plugins/dashboard-plugin';
+import Clipboard from 'v-clipboard'
+import App from './App.vue';
+// router setup
+import router from './routes/router';
+import i18n from './i18n';
+import './registerServiceWorker'
+import store from './store'
+
+import eventBus from '@/plugins/event-bus'
+import synchronizer from './plugins/synchronizer'
+
+Vue.config.productionTip = false;
+Vue.prototype.$eventBus = eventBus
+
+// plugin setup
+Vue.use(DashboardPlugin)
+Vue.use(VueRouter)
+Vue.use(RouterPrefetch)
+Vue.use(Clipboard)
+Vue.use(synchronizer)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router,
+  store,
+  i18n
+});
