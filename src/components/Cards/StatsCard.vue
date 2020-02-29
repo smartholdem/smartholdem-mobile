6b24f074af3c8905b1094854b9dd2c9207c3afae
technolog <technologies.bro@gmail.com>
2020-02-29 23:56:36 +0300

    cards

---
+++ src/components/Cards/StatsCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <card class="card-stats" :show-footer-line="true">
+    <div class="row">
+      <div class="col-5" v-if="$slots.icon || icon">
+        <div class="info-icon text-center" :class="`icon-${type}`">
+          <slot name="icon"> <i :class="icon"></i> </slot>
+        </div>
+      </div>
+      <div class="col-7" v-if="$slots.content || title || subTitle">
+        <div class="numbers">
+          <slot>
+            <p v-if="subTitle" class="card-category">{{ subTitle }}</p>
+            <h3 v-if="title" class="card-title">{{ title }}</h3>
+          </slot>
+        </div>
+      </div>
+    </div>
+    <div class="stats" slot="footer" v-if="$slots.footer">
+      <slot name="footer"></slot>
+    </div>
+  </card>
+</template>
+<script>
+import Card from './Card.vue';
+
+export default {
+  name: 'stats-card',
+  components: {
+    Card
+  },
+  props: {
+    type: {
+      type: String,
+      default: 'primary'
+    },
+    icon: String,
+    title: String,
+    subTitle: String
+  }
+};
+</script>
+<style>
+  .card-stats .info-icon.icon-gray {
+    background: #fd5d93;
+    background-image: -webkit-gradient(linear, right top, left bottom, from(#171415), color-stop(#9E9E9E), to(#E91E63));
+    background-image: linear-gradient(to bottom left, #171415, #9E9E9E, #E91E63);
+    background-size: 210% 210%;
+    background-position: top right;
+  }
+</style>
