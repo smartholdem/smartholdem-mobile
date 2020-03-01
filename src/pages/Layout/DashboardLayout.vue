0c2bd716446f192520d6dc5b6932e2e06956ed73
technolog <technologies.bro@gmail.com>
2020-03-01 21:22:26 +0300

    dashboard:layout

---
+++ src/pages/Layout/DashboardLayout.vue
@@ -0,0 +1,409 @@
+<template>
+  <div>
+    <!--<system-menu/>-->
+    <div class="wrapper" :class="{ 'nav-open': $sidebar.showSidebar }">
+      <notifications></notifications>
+      <sidebar-fixed-toggle-button/>
+      <side-bar
+        :background-color="sidebarBackground"
+        :short-title="$t('sidebar.shortTitle')"
+        :title="$t('sidebar.title')"
+      >
+        <template slot-scope="props" slot="links">
+          <sidebar-item
+            v-if="!$root.isMobile"
+            :link="{
+            name: $t('sidebar.dashboard'),
+            icon: 'tim-icons icon-chart-bar-32',
+            path: '/dashboard'
+          }"
+          >
+          </sidebar-item>
+
+          <sidebar-item
+            :link="{
+            name: 'Wallets',
+            icon: 'tim-icons icon-wallet-43',
+            path: '/wallet'
+          }"
+          >
+          </sidebar-item>
+
+          <span v-if="!$root.isMobile">
+          <sidebar-item
+            :link="{
+            name: 'Contacts',
+            icon: 'tim-icons icon-book-bookmark',
+            path: '/contacts'
+          }"
+          >
+          </sidebar-item>
+
+          <sidebar-item
+            :link="{
+            name: 'SmartHolder',
+            icon: 'tim-icons icon-money-coins',
+            path: '/smartholder'
+          }"
+          >
+          </sidebar-item>
+
+          <sidebar-item
+            :link="{
+            name: 'Vote listing XBTS',
+            icon: 'tim-icons icon-trophy',
+            path: '/xbts-listing'
+          }"
+          >
+          </sidebar-item>
+</span>
+
+          <!--
+          <sidebar-item
+            :link="{
+            name: $t('sidebar.components'),
+            icon: 'tim-icons icon-molecule-40'
+          }"
+          >
+            <sidebar-item :link="{ name: $t('sidebar.multiLevelCollapse') }">
+              <sidebar-item
+                :link="{
+                name: $t('sidebar.example'),
+                isRoute: false,
+                path: 'https://google.com',
+                target: '_blank'
+              }"
+              ></sidebar-item>
+            </sidebar-item>
+
+            <sidebar-item
+              :link="{ name: $t('sidebar.buttons'), path: '/components/buttons' }"
+            ></sidebar-item>
+            <sidebar-item
+              :link="{
+              name: $t('sidebar.gridSystem'),
+              path: '/components/grid-system'
+            }"
+            ></sidebar-item>
+            <sidebar-item
+              :link="{ name: $t('sidebar.panels'), path: '/components/panels' }"
+            ></sidebar-item>
+            <sidebar-item
+              :link="{
+              name: $t('sidebar.sweetAlert'),
+              path: '/components/sweet-alert'
+            }"
+            ></sidebar-item>
+            <sidebar-item
+              :link="{
+              name: $t('sidebar.notifications'),
+              path: '/components/notifications'
+            }"
+            ></sidebar-item>
+            <sidebar-item
+              :link="{ name: $t('sidebar.icons'), path: '/components/icons' }"
+            ></sidebar-item>
+            <sidebar-item
+              :link="{
+              name: $t('sidebar.typography'),
+              path: '/components/typography'
+            }"
+            ></sidebar-item>
+          </sidebar-item>
+          -->
+          <!--
+          <sidebar-item
+            :link="{ name: $t('sidebar.forms'), icon: 'tim-icons icon-notes' }"
+          >
+            <sidebar-item
+              :link="{ name: $t('sidebar.regularForms'), path: '/forms/regular' }"
+            ></sidebar-item>
+            <sidebar-item
+              :link="{
+              name: $t('sidebar.extendedForms'),
+              path: '/forms/extended'
+            }"
+            ></sidebar-item>
+            <sidebar-item
+              :link="{
+              name: $t('sidebar.validationForms'),
+              path: '/forms/validation'
+            }"
+            ></sidebar-item>
+            <sidebar-item
+              :link="{ name: $t('sidebar.wizard'), path: '/forms/wizard' }"
+            ></sidebar-item>
+          </sidebar-item>
+          <sidebar-item
+            :link="{
+            name: $t('sidebar.tables'),
+            icon: 'tim-icons icon-puzzle-10'
+          }"
+          >
+            <sidebar-item
+              :link="{
+              name: $t('sidebar.regularTables'),
+              path: '/table-list/regular'
+            }"
+            ></sidebar-item>
+            <sidebar-item
+              :link="{
+              name: $t('sidebar.extendedTables'),
+              path: '/table-list/extended'
+            }"
+            ></sidebar-item>
+            <sidebar-item
+              :link="{
+              name: $t('sidebar.paginatedTables'),
+              path: '/table-list/paginated'
+            }"
+            ></sidebar-item>
+          </sidebar-item>
+          <sidebar-item
+            :link="{
+            name: $t('sidebar.charts'),
+            icon: 'tim-icons icon-chart-bar-32',
+            path: '/charts'
+          }"
+          ></sidebar-item>
+          -->
+        </template>
+      </side-bar>
+
+      <div class="main-panel" :data="sidebarBackground">
+        <dashboard-navbar></dashboard-navbar>
+        <router-view name="header"></router-view>
+
+        <div
+          :class="{ content: !$route.meta.hideContent }"
+          @click="toggleSidebar"
+        >
+          <zoom-center-transition :duration="200" mode="out-in">
+            <!-- sth content here -->
+            <router-view></router-view>
+          </zoom-center-transition>
+        </div>
+        <!--<content-footer v-if="!$route.meta.hideFooter"></content-footer>-->
+      </div>
+    </div>
+
+    <!-- global modals -->
+    <div v-if="modal.label.show">
+    <ModalSetLabel :showModal="modal.label.show" :address="modal.label.address" @onModalClose="modal.label.show = false" />
+    </div>
+
+    <ModalUnlock :modalPin="modal.unlock.show" @onUnlockClose="modal.unlock.show = false"/>
+
+    <div v-if="modal.contacts.show">
+    <ModalAddContact :showModal="modal.contacts.show" @onModalClose="modal.contacts.show = false"/>
+    </div>
+
+    <div v-if="modal.vote.show">
+    <ModalTxVote :showModalVote="modal.vote.show" :voteData="modal.vote.data" @onModalClose="modal.vote.show = false"/>
+    </div>
+
+    <div v-if="modal.send.show">
+    <ModalTxSend :showModal="modal.send.show" :address="modal.send.address" @onModalClose="modal.send.show = false"/>
+    </div>
+
+  </div>
+
+</template>
+<script>
+/* eslint-disable no-new */
+import PerfectScrollbar from 'perfect-scrollbar';
+import 'perfect-scrollbar/css/perfect-scrollbar.css';
+
+function hasElement(className) {
+  return document.getElementsByClassName(className).length > 0;
+}
+
+function initScrollbar(className) {
+  if (hasElement(className)) {
+    new PerfectScrollbar(`.${className}`);
+  } else {
+    // try to init it later in case this component is loaded async
+    setTimeout(() => {
+      initScrollbar(className);
+    }, 100);
+  }
+}
+
+import DashboardNavbar from './DashboardNavbar.vue';
+import ContentFooter from './ContentFooter.vue';
+import DashboardContent from './Content.vue';
+import SidebarFixedToggleButton from './SidebarFixedToggleButton.vue';
+import {SlideYDownTransition, ZoomCenterTransition} from 'vue2-transitions';
+//import SystemMenu from 'src/components/SystemMenu.vue'
+import ModalSetLabel from '@/components/Wallet/ModalSetLabel'
+import ModalUnlock from '@/components/Wallet/Unlock'
+import ModalAddContact from '@/components/Wallet/ModalAddContact'
+import ModalTxSend from '@/components/Wallet/ModalTxSend'
+import ModalTxVote from '@/components/Wallet/ModalTxVote'
+
+export default {
+  components: {
+    DashboardNavbar,
+    ContentFooter,
+    SidebarFixedToggleButton,
+    DashboardContent,
+    SlideYDownTransition,
+    ZoomCenterTransition,
+//    SystemMenu,
+    ModalSetLabel,
+    ModalTxVote,
+    ModalUnlock,
+    ModalAddContact,
+    ModalTxSend,
+  },
+  data() {
+    return {
+      modal: {
+        label: {
+          show: false,
+          address: ''
+        },
+        vote: {
+          show: false,
+          data: {
+            username: '',
+          }
+        },
+        unlock: {
+          show: false,
+        },
+        contacts: {
+          show: false,
+        },
+        send: {
+          show: false,
+          address: ''
+        },
+      },
+      sidebarBackground: 'red' //vue|blue|orange|green|red|primary
+    };
+  },
+
+  async created() {
+    if (!this.$store.getters['app/pinEncrypted']) {
+      this.$router.push({path: '/register'})
+    } else if (!this.$root.pin) {
+      //this.$router.push({path: '/lock'})
+    }
+    await this.getFees()
+
+    this.$eventBus.on('close:modal', async () => {
+      this.modal = {
+        label: {
+          show: false,
+          address: ''
+        },
+        vote: {
+          show: false,
+          data: {}
+        },
+        unlock: {
+          show: false,
+        },
+        contacts: {
+          show: false,
+        },
+        send: {
+          show: false,
+          address: ''
+        },
+      }
+    })
+
+    this.$eventBus.on('modal:label', async (address) => {
+      this.modal.label.show = false
+      this.modal.label.address = address
+      this.modal.label.show = true
+      await this.$store.dispatch('wallet/getBalances')
+    })
+
+    this.$eventBus.on('modal:unlock', async () => {
+      this.modal.unlock.show = false
+      this.modal.unlock.show = true
+    })
+
+    this.$eventBus.on('modal:vote', async (data) => {
+      this.modal.vote.show = false
+      this.modal.vote.data = data
+      this.modal.vote.show = true
+    })
+
+    this.$eventBus.on('modal:contacts', async () => {
+      this.modal.contacts.show = false
+      this.modal.contacts.show = true
+    })
+
+    this.$eventBus.on('modal:send', async (address) => {
+      this.modal.send.show = false
+      this.modal.send.address = address
+      this.modal.send.show = true
+    })
+
+
+    await this.$store.dispatch('network/getSeed')
+
+  },
+  methods: {
+    toggleSidebar() {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.displaySidebar(false);
+      }
+    },
+    initScrollbar() {
+      let docClasses = document.body.classList;
+      let isWindows = navigator.platform.startsWith('Win');
+      if (isWindows) {
+        // if we are on windows OS we activate the perfectScrollbar function
+        initScrollbar('sidebar');
+        initScrollbar('main-panel');
+        initScrollbar('sidebar-wrapper');
+
+        docClasses.add('perfect-scrollbar-on');
+      } else {
+        docClasses.add('perfect-scrollbar-off');
+      }
+    },
+    async getFees() {
+      await this.$store.dispatch('blockchain/getFees')
+    }
+  },
+  mounted() {
+    // this.initScrollbar();
+  }
+};
+</script>
+<style lang="scss">
+  $scaleSize: 0.95;
+  @keyframes zoomIn95 {
+    from {
+      opacity: 0;
+      transform: scale3d($scaleSize, $scaleSize, $scaleSize);
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  .main-panel .zoomIn {
+    animation-name: zoomIn95;
+  }
+
+  @keyframes zoomOut95 {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+      transform: scale3d($scaleSize, $scaleSize, $scaleSize);
+    }
+  }
+
+  .main-panel .zoomOut {
+    animation-name: zoomOut95;
+  }
+</style>
