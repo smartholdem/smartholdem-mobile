20d68669e424d8ab1a0aab4929f67f972016e5de
technolog <technologies.bro@gmail.com>
2020-03-01 16:49:47 +0300

    xbts plugins

---
+++ src/components/Exchange/XbtsListCandidates.vue
@@ -0,0 +1,73 @@
+<template>
+  <card card-body-classes="table-full-width" style="margin-bottom: 0px;">
+    <!--<h4 slot="header" class="card-title">{{$t('WALLET.TRANSACTIONS')}} <span class="font-weight-bold">{{totalTx}}</span></h4>-->
+
+    <!-- tx desktop -->
+    <el-table v-if="!$root.isMobile" :data="candidates" :maxHeight="$root.height - 512" stripe>
+      <el-table-column
+        className=""
+        min-width="20"
+        label="Ticker"
+      >
+        <div class="" slot-scope="{ row }">
+          {{row.ticker}}
+        </div>
+      </el-table-column>
+
+      <el-table-column
+        className=""
+        min-width="100"
+        label="Coin"
+      >
+        <div class="text-darker" slot-scope="{ row }">
+          {{row.name}}
+        </div>
+      </el-table-column>
+
+      <el-table-column
+        className=""
+        min-width="100"
+        label="Vote Address"
+      >
+        <div class="font-weight-bold text-darker" slot-scope="{ row }"  v-clipboard="() => row.address">
+          <i class="tim-icons icon-single-copy-04 pointer"></i> {{row.address}}
+        </div>
+      </el-table-column>
+
+      <el-table-column
+        className=""
+        min-width="100"
+        label="Votes STH"
+      >
+        <div class="font-weight-bold" slot-scope="{ row }">
+          {{row.weight}}
+        </div>
+      </el-table-column>
+    </el-table>
+
+  </card>
+</template>
+
+<script>
+import {Table, TableColumn} from 'element-ui'
+
+export default {
+  name: "XbtsListCandidates",
+  components: {
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn,
+  },
+  computed: {
+    candidates() {
+      return this.$store.getters['blockchain/xbtsCandidates']
+    },
+  },
+  async created() {
+    await this.$store.dispatch('blockchain/getXbtsCandidates')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Exchange/XbtsListWinners.vue
@@ -0,0 +1,101 @@
+<template>
+  <card card-body-classes="table-full-width" style="margin-bottom: 0px;">
+    <!--<h4 slot="header" class="card-title">{{$t('WALLET.TRANSACTIONS')}} <span class="font-weight-bold">{{totalTx}}</span></h4>-->
+
+    <!-- tx desktop -->
+    <el-table v-if="!$root.isMobile" :data="winners" :maxHeight="$root.height - 450" stripe>
+
+      <el-table-column
+        className=""
+        min-width="10"
+        label="#"
+      >
+        <div class="" slot-scope="{ row }">
+          <img width="32px" :src="'https://ex.xbts.io/asset-symbols/'+row.ticker.toLowerCase()+'.png'"/>
+        </div>
+      </el-table-column>
+
+      <el-table-column
+        className=""
+        min-width="20"
+        label="Ticker"
+      >
+        <div class="" slot-scope="{ row }">
+          {{row.ticker}}
+        </div>
+      </el-table-column>
+
+      <el-table-column
+        className=""
+        min-width="50"
+        label="Coin"
+      >
+        <div class="text-uppercase" slot-scope="{ row }">
+          {{row.name}}
+        </div>
+      </el-table-column>
+
+      <el-table-column
+        className=""
+        min-width="50"
+        label="State"
+      >
+        <div class="text-uppercase mt-1" slot-scope="{ row }">
+          <span v-if="row.state === 'listed'">{{row.state}}
+            <base-button @click="openTicker(row.ticker.toUpperCase())" type="danger" round icon simple class="ml-2">
+                  <el-tooltip
+                    content="Trade Now"
+                    effect="light"
+                    :open-delay="300"
+                    placement="top"
+                  >
+                    <i class="tim-icons icon-chart-bar-32" style="font-size: 1rem"></i>
+                  </el-tooltip>
+                </base-button>
+
+            </span>
+          <span v-if="row.state === 'winner'">{{row.state}}</span>
+        </div>
+      </el-table-column>
+
+    </el-table>
+
+  </card>
+</template>
+
+<script>
+import {Table, TableColumn} from 'element-ui'
+import {openUrl} from 'src/util/url'
+
+export default {
+  name: "XbtsListCandidates",
+  components: {
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn,
+  },
+  methods: {
+    openTicker(ticker) {
+      openUrl('https://ex.xbts.io/#/market/XBTSX.'+ticker+'_BTS')
+    }
+  },
+  computed: {
+    winners() {
+      let result = []
+      const data = this.$store.getters['blockchain/xbtsWinners']
+      for (let i=0; i < data.length; i++) {
+        if (data[i].state === 'listed' || data[i].state === 'winner') {
+          result.push(data[i])
+        }
+      }
+      return result
+    },
+  },
+  async created() {
+    await this.$store.dispatch('blockchain/getXbtsWinners')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
