a1569c2dd226103cc5df504e56666491ee3848b4
technolog <technologies.bro@gmail.com>
2020-03-01 23:03:11 +0300

    validatePin

--- src/pages/Layout/DashboardLayout.vue
+++ src/pages/Layout/DashboardLayout.vue
@@ -188,8 +188,9 @@
     </div>
 
 
-    <div class="pin-layer p-2">
-      <span class="float-right"><i class="tim-icons icon-simple-remove" style="font-size: 1.2em;"></i></span>
+    <div v-if="modal.unlock.show" class="pin-layer p-2">
+      <span @click="closePinLayer" class="float-right"><i class="tim-icons icon-simple-remove"
+                                                          style="font-size: 1.2em;"></i></span>
       <h4 class="text-center">Enter PIN-Code</h4>
       <div class="p-4 pl-5 mt-3">
         <div class="fill-pin w-100 p-2 ml-4 float-left mb-4">
@@ -201,16 +202,26 @@
           <span :class="'fill-pin-item ml-2 ' + (pin.length > 5 ? 'pin-filled' : '')"></span>
         </div>
 
-        <base-button @click="pinPress('1')" type="default" round simple class="pin-btn font-weight-light">1</base-button>
-        <base-button @click="pinPress('2')" type="default" round simple class="pin-btn font-weight-light">2</base-button>
-        <base-button @click="pinPress('3')" type="default" round simple class="pin-btn font-weight-light">3</base-button>
-        <base-button @click="pinPress('4')" type="default" round simple class="pin-btn font-weight-light">4</base-button>
-        <base-button @click="pinPress('5')" type="default" round simple class="pin-btn font-weight-light">5</base-button>
-        <base-button @click="pinPress('6')" type="default" round simple class="pin-btn font-weight-light">6</base-button>
-        <base-button @click="pinPress('7')" type="default" round simple class="pin-btn font-weight-light">7</base-button>
-        <base-button @click="pinPress('8')" type="default" round simple class="pin-btn font-weight-light">8</base-button>
-        <base-button @click="pinPress('9')" type="default" round simple class="pin-btn font-weight-light">9</base-button>
-        <base-button @click="pinPress('0')" type="default" round simple class="pin-btn font-weight-light">0</base-button>
+        <base-button @click="pinPress('1')" type="default" round simple class="pin-btn font-weight-light">1
+        </base-button>
+        <base-button @click="pinPress('2')" type="default" round simple class="pin-btn font-weight-light">2
+        </base-button>
+        <base-button @click="pinPress('3')" type="default" round simple class="pin-btn font-weight-light">3
+        </base-button>
+        <base-button @click="pinPress('4')" type="default" round simple class="pin-btn font-weight-light">4
+        </base-button>
+        <base-button @click="pinPress('5')" type="default" round simple class="pin-btn font-weight-light">5
+        </base-button>
+        <base-button @click="pinPress('6')" type="default" round simple class="pin-btn font-weight-light">6
+        </base-button>
+        <base-button @click="pinPress('7')" type="default" round simple class="pin-btn font-weight-light">7
+        </base-button>
+        <base-button @click="pinPress('8')" type="default" round simple class="pin-btn font-weight-light">8
+        </base-button>
+        <base-button @click="pinPress('9')" type="default" round simple class="pin-btn font-weight-light">9
+        </base-button>
+        <base-button @click="pinPress('0')" type="default" round simple class="pin-btn font-weight-light">0
+        </base-button>
         <span @click="pinPress('del')" class="pin-btn"><i class="fas fa-backspace pt-3"></i></span>
         <!--<span class="pin-btn"><i class="tim-icons icon-check-2 pt-3"></i></span>-->
 
@@ -382,11 +393,26 @@ export default {
 
   },
   methods: {
+    async validatePin() {
+      if (this.pin.length > 3 && this.$store.getters['app/pinEncrypted']) {
+        let decryptPin = await this.$store.dispatch('app/validatePinCode', this.pin)
+        if (decryptPin) {
+          this.$root.pin = decryptPin
+          this.modal.unlock.show = false
+          this.pin = ''
+        }
+      }
+    },
+    async closePinLayer() {
+      this.modal.unlock.show = false
+      this.pin = ''
+    },
     async pinPress(num) {
       if (num === 'del') {
         this.pin = this.pin.substr(0, this.pin.length - 1)
       } else {
         this.pin = this.pin + num
+        await this.validatePin()
       }
       console.log('this.pin', this.pin)
     },
