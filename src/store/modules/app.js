eb0d6e082af9f864c7c2f82e15060bb4731d0e97
technolog <technologies.bro@gmail.com>
2020-03-01 16:31:05 +0300

    store:app

---
+++ src/store/modules/app.js
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2020 TechnoL0g.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+import i18n from '@/i18n'
+import CryptoJS from 'crypto-js'
+import releaseService from '@/services/release'
+import Blockchain from '@/services/blockchain'
+
+export default {
+  namespaced: true,
+
+  state: {
+    language: 'en',
+    pinEncrypted: null, // encrypted hash
+    settings: {
+      sound: true,
+    },
+    accounts: [], // здесь будем хранить много адресов ключ = address > SHA384 > AES,
+    contacts: {}, // contact list
+  },
+
+  getters: {
+    language: state => state.language,
+    pinEncrypted: state => state.pinEncrypted,
+    latestReleaseVersion: state => state.latestReleaseVersion,
+    settings: state => state.settings,
+    accounts: state => state.accounts,
+    contacts: state => state.contacts,
+  },
+
+  mutations: {
+    SET_SETTINGS (state, payload) {
+      state.settings = payload
+    },
+    SET_LATEST_RELEASE_VERSION (state, version) {
+      state.latestReleaseVersion = version
+    },
+    SET_RESET (state) {
+      state.language = 'en'
+      state.pinEncrypted = null
+      state.accounts = []
+    },
+    SET_LANGUAGE (state, payload) {
+      state.language = payload
+    },
+    SET_PIN_ENC (state, payload) {
+      // полученный хэш шифруем тем же хэшем
+      const hash = CryptoJS.SHA384(payload).toString()
+      state.pinEncrypted = (CryptoJS.AES.encrypt(hash, hash)).toString() // msg, key
+    },
+    SET_ACCOUNT (state, payload) {
+      //const hashPin = CryptoJS.SHA384(payload.pin).toString()
+      const hashPin = payload.pin
+      state.accounts[payload.address] = {
+        address: payload.address,
+        secret: (CryptoJS.AES.encrypt(payload.secret, hashPin)).toString(),
+        pubKey: payload.pubKey,
+      }
+    },
+    UPD_ACCOUNTS(state, payload) {
+      state.accounts = payload
+    },
+    SET_CONTACT(state, payload) {
+      state.contacts[payload.address] = payload
+    },
+    UPD_CONTACTS(state, payload) {
+      state.contacts = payload
+    },
+  },
+
+  actions: {
+    async setSettings ({ commit }, value) {
+      commit('SET_SETTINGS', value)
+    },
+    async checkNewVersion ({ commit }) {
+      const latestRelease = await releaseService.fetchLatestRelease()
+      // eslint-disable-next-line camelcase
+      commit('SET_LATEST_RELEASE_VERSION', latestRelease.tag_name)
+    },
+    appReset ({ commit }) {
+      commit('SET_RESET')
+    },
+    setLanguage ({ commit }, value) {
+      commit('SET_LANGUAGE', value)
+      i18n.locale = value
+    },
+    setPinEnc ({ commit }, value) {
+      commit('SET_PIN_ENC', value)
+    },
+    setAccount ({ commit }, value) {
+      commit('SET_ACCOUNT', value)
+    },
+    async updateAccounts({ commit }, value) {
+      commit('UPD_ACCOUNTS', value)
+    },
+    async walletDecrypt({ commit }, value) {
+      //let currentHashPin = (CryptoJS.SHA384(value.pin)).toString()
+      const accounts = await this.getters['app/accounts']
+      let accountBytes = CryptoJS.AES.decrypt(accounts[value.address].secret.toString(), value.pin)
+      return accountBytes.toString(CryptoJS.enc.Utf8)
+    },
+    async setContact({commit}, value) {
+        commit('SET_CONTACT', {
+          address: value.address,
+          label: value.label,
+          balance: await Blockchain.getAddressBalance(value.address),
+        })
+    },
+    async updateContacts({ commit }, value) {
+      commit('UPD_CONTACTS', value)
+    },
+    async fetchContacts({ commit }) {
+      const contacts = await this.getters['app/contacts']
+      let result = {}
+      let keys = Object.keys(contacts)
+      for (let i=0; i < keys.length; i++) {
+        result[keys[i]] = {
+          address: contacts[keys[i]].address,
+          label: contacts[keys[i]].label,
+          balance: await Blockchain.getAddressBalance(contacts[keys[i]].address),
+        }
+      }
+      commit('UPD_CONTACTS', result)
+    }
+  }
+}
