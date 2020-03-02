66d2f9b6e7689c5714ad1ab7cfc92a7fa3b0502c
technolog <technologies.bro@gmail.com>
2020-03-02 09:17:34 +0300

    Welcome

--- src/pages/Pages/Welcome.vue
+++ src/pages/Pages/Welcome.vue
@@ -1,7 +1,20 @@
 <template>
-    <div>
+  <div>
+  <div class="p-0 m-0 w-100 position-fixed">
+      <div class="text-center h-100 mt-5">
+      <img clas="" src="images/sth80.png"/>
+        <h2 class="text-uppercase mt-3 font-weight-bold">
+          SmartHoldem
+          <br/>WALLET
+        </h2>
+        <p>Welcome to the Smartholdem Platform</p>
 
+        <div class="text-center w-100 m-auto pt-3">
+          <base-button type="primary" round class="text-uppercase">Create Wallet</base-button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +23,9 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style>
+  .full-page > .content {
+    padding: 0;
+    margin:0;
+  }
 </style>
