daf3f71cd08729b23180fc6286ed41821f753de7
technolog <technologies.bro@gmail.com>
2020-03-05 07:32:07 +0300

    delegates page

--- src/pages/Dashboard/Delegates.vue
+++ src/pages/Dashboard/Delegates.vue
@@ -1,20 +1,54 @@
 <template>
   <div>
-    <Delegates/>
+    <h4>Delegates</h4>
+    <table class="delegate-table w-100">
+      <tr v-for="(item, idx) in Delegates.list" :key="idx" class="">
+        <td>
+          {{item.rate}}
+        </td>
+        <td>
+          {{item.username}}
+        </td>
+        <td>
+          {{item.approval}}%
+        </td>
+      </tr>
+    </table>
+
   </div>
 </template>
 
 <script>
-import Delegates from '@/components/Blockchain/Delegates'
 
 export default {
   name: "Delegates",
   components: {
-    Delegates,
-  }
+  //  Delegates,
+  },
+  computed: {
+    Delegates() {
+      let result = {
+        count: 0,
+        list: [],
+      }
+      const data = this.$store.getters['blockchain/delegates']
+      console.log(data)
+      result.count = data.count
+      result.list = data.list
+      return result
+    },
+  },
 }
 </script>
 
 <style scoped>
+
+  .delegate-table td {
+    padding: 5px;
+  }
+
+  .delegate-table tr {
+    border-bottom: solid 1px #2f253575
+  }
 
 </style>
