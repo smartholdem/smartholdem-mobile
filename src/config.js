c4a1586bd82811c3a9931c1ef43fae9ad5070c0b
technolog <technologies.bro@gmail.com>
2020-03-07 22:13:03 +0300

    config

---
+++ src/config.js
@@ -0,0 +1,50 @@
+exports.colors = {
+    default: '#000000',
+    primary: '#42b883',
+    info: '#1d8cf8',
+    danger: '#fd5d93',
+    orange: '#ff8a76',
+    teal: '#00d6b4',
+    primaryGradient: [
+      'rgba(76, 211, 150, 0.1)',
+      'rgba(53, 183, 125, 0)',
+      'rgba(119,52,169,0)'
+    ],
+    purpleGradient: ['rgba(253,93,147,0.8)', 'rgba(253,93,147,0)']
+};
+
+
+exports.network = {
+  EXPLORER: 'https://blockexplorer.smartholdem.io/#/',
+  AGENT: 'sth-wallet',
+  XBTS_VOTE: 'https://vote.xbts.io/api/',
+  SMARTHOLDER: 'https://smartholder.xbts.io',
+  NODE: 'node0.smartholdem.io',
+  PEERS: [
+    '80.211.31.231',
+    '212.237.17.171',
+    '213.239.207.170',
+    '94.177.213.55',
+    '80.211.32.86',
+    '188.213.168.227',
+    '194.182.68.74',
+    '194.182.74.218',
+    '194.182.74.212',
+    '194.182.74.130',
+    '194.182.74.228',
+    '194.182.66.20',
+    '80.211.208.154',
+    '80.211.220.200',
+  ],
+  SEEDS: [
+    'node0.smartholdem.io',
+    'node1.smartholdem.io',
+    'node2.smartholdem.io',
+    'node3.smartholdem.io',
+    'node4.smartholdem.io',
+  ]
+}
+
+exports.exchange = {
+  API: 'https://ex.smartholdem.io'
+}
