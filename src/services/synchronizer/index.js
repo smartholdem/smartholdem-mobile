40fc27e734dd7d35fabb5b4615ccb2e0201d2aec
technolog <technologies.bro@gmail.com>
2020-03-07 22:19:35 +0300

    sync:index

---
+++ src/services/synchronizer/index.js
@@ -0,0 +1,21 @@
+import balance from './balance'
+import tx from './tx'
+import market from './market'
+import status from './status'
+import priceHistory from './priceHistory'
+import holders from './holders'
+import assetsPay from './assets-pay'
+import assetsNoq from './assets-noq'
+//import ledger from './ledger'
+
+export {
+  balance,
+  tx,
+  market,
+  status,
+  priceHistory,
+  holders,
+  assetsPay,
+  assetsNoq,
+//  ledger,
+}
