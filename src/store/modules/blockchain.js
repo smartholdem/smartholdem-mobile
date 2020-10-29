d02f1c986266dd49a5bc0c08d22452da57d84b28
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2020-10-29 14:33:26 +0300

    gettter:standbyDelegates

--- src/store/modules/blockchain.js
+++ src/store/modules/blockchain.js
@@ -57,6 +57,7 @@ export default {
 
   getters: {
     delegates: state => state.delegates,
+    standbyDelegates: state => state.standbyDelegates,
     status: state => state.status,
     fees: state => state.fees,
     xbtsCandidates: state => state.xbtsCandidates,
