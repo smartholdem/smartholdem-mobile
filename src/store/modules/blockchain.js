4b7f2199496ed3e651f22f1ad2ea371f3ac443ed
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2020-10-29 14:33:54 +0300

    SET_DELEGATES_STANDBY

--- src/store/modules/blockchain.js
+++ src/store/modules/blockchain.js
@@ -69,6 +69,9 @@ export default {
     SET_DELEGATES(state, payload) {
       state.delegates = payload
     },
+    SET_DELEGATES_STANDBY(state, payload) {
+      state.standbyDelegates = payload
+    },
     SET_STATUS(state, payload) {
       state.status = payload
     },
